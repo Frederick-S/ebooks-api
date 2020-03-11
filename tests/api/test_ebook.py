e2941046bb2098e6f26fec131525397f034acf37
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-11 11:39:29 +0800

    Update test

--- tests/api/test_ebook.py
+++ tests/api/test_ebook.py
@@ -8,3 +8,11 @@ class NotebookTestCase(BaseTestCase):
 
         self.assertEqual(200, response.status_code)
         self.assertTrue(len(response.json) > 0)
+
+    def test_get_ebooks_with_invalid_parameters(self):
+        response1 = self.client.get('/api/v1/ebooks')
+        response2 = self.client.get(
+            '/api/v1/ebooks?provider=weread')
+
+        self.assertEqual(400, response1.status_code)
+        self.assertEqual(400, response2.status_code)
