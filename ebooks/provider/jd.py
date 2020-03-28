6dd823d2bfc98385e4d8c9a3013c5250b643543b
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-28 19:22:34 +0800

    fix: jd - book image is empty

--- ebooks/provider/jd.py
+++ ebooks/provider/jd.py
@@ -23,9 +23,10 @@ class JDEbookProvider(EbookProvider):
         return list(map(self.__convert_to_ebook, books))
 
     def __convert_to_ebook(self, book):
+        image = book.find('.p-img img')
         ebook = Ebook()
         ebook.title = book.find('.p-name').text()
         ebook.author = book.find('.p-bi-name a[title]').text()
-        ebook.cover = book.find('.p-img img').attr('src')
+        ebook.cover = image.attr('src') or image.attr('data-lazy-img')
 
         return ebook
