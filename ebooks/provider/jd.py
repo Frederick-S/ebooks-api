b88d265c08bf0ecfe5dd6657fc8a78a78eac7811
Frederick-S <mao_xiaodan@hotmail.com>
2020-03-28 19:45:07 +0800

    fix: jd - get book price

--- ebooks/provider/jd.py
+++ ebooks/provider/jd.py
@@ -19,14 +19,30 @@ class JDEbookProvider(EbookProvider):
         document = PyQuery(response.text)
         books = [PyQuery(book) for book in
                  document.find('.goods-list-ebook li')]
+        sku_ids = list(map(self.__get_sku_id, books))
+        prices = self.__get_prices(sku_ids)
 
-        return list(map(self.__convert_to_ebook, books))
+        return list(map(
+            lambda book: self.__convert_to_ebook(book, prices), books))
 
-    def __convert_to_ebook(self, book):
+    def __convert_to_ebook(self, book, prices):
+        sku_id = self.__get_sku_id(book)
         image = book.find('.p-img img')
+
         ebook = Ebook()
         ebook.title = book.find('.p-name').text()
         ebook.author = book.find('.p-bi-name a[title]').text()
+        ebook.price = prices.get(sku_id, 0.0)
         ebook.cover = image.attr('src') or image.attr('data-lazy-img')
 
         return ebook
+
+    def __get_sku_id(self, book):
+        return 'J_' + book.attr('data-sku')
+
+    def __get_prices(self, sku_ids):
+        url = 'https://p.3.cn/prices/mgets?skuIds={}'.format(','.join(sku_ids))
+        response = requests.get(url)
+        data = response.json()
+
+        return {x.get('id'): x.get('p') for x in data}
