5d8edadd04f951e4c74546e630e9485531f82f9a
Frederick-S <mao_xiaodan@hotmail.com>
2021-08-08 17:42:43 +0800

    fix #19

--- ebooks/provider/turing.py
+++ ebooks/provider/turing.py
@@ -1,30 +1,41 @@
 import requests
-from pyquery import PyQuery
 from ebooks.provider.ebook import Ebook
 from ebooks.provider.ebook_provider import EbookProvider
 
 
 class TuringEbookProvider(EbookProvider):
     def __init__(self):
-        self.url = 'https://www.ituring.com.cn/menu/books?q={}&page={}'
+        self.url = 'https://api.ituring.com.cn/api/Search/Advanced'
 
     def get_ebooks(self, title, last_book_index, page_index):
-        url = self.url.format(title, page_index)
-        response = requests.get(url)
+        data = {
+            'categoryId': 0,
+            'edition': 4,
+            'name': title,
+            'page': page_index
+        }
+        response = requests.post(self.url, json=data)
 
         if response.status_code != requests.codes.ok:
             raise Exception(response.text)
 
-        document = PyQuery(response.text)
-        books = [PyQuery(book) for book in document.find('.block-item')]
+        body = response.json()
 
-        return list(map(self.__convert_to_ebook, books))
+        return list(map(self.__convert_to_ebook, body.get('bookItems', {})))
 
     def __convert_to_ebook(self, book):
         ebook = Ebook()
-        ebook.title = book.find('.name').text()
-        ebook.author = book.find('.author').text()
-        ebook.cover = book.find('.book-img img').attr('src')
-        ebook.intro = book.find('.intro').text()
+        ebook.title = book.get('name', '')
+        ebook.author = book.get('authorNameString', '')
+        ebook.price = self.__get_price(book.get('bookEditionPrices', []))
+        ebook.cover = 'https://file.ituring.com.cn/LargeCover/{}'.format(book.get('coverKey', ''))
+        ebook.intro = book.get('abstract', '')
 
         return ebook
+
+    def __get_price(self, prices):
+        for price in prices:
+            if price.get('key', '') == 'Ebook':
+                return price.get('name', '')
+
+        return 0.0
